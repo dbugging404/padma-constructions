64c0e46b82961973c7813ec4ab0ff53f306a32e6
dbugging404 <avinashmishra13@hotmail.com>
2024-03-04 12:33:28 +0530

    Add placeholder attributes to Typography and Button components

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -10,17 +10,21 @@ const CTA = () => {
           variant='small'
           color='blue-gray'
           className='font-bold mb-2'
+          placeholder={''}
         >
           Upcoming Events
         </Typography>
-        <Typography variant='h3' color='blue-gray'>
+        <Typography variant='h3' color='blue-gray' placeholder={''}>
           Tech Summit: Shaping Tomorrow
         </Typography>
-        <Typography className='mt-2 mb-6 !text-base font-normal text-gray-500'>
+        <Typography
+          className='mt-2 mb-6 !text-base font-normal text-gray-500'
+          placeholder={''}
+        >
           Prepare to be part of dynamic conversations that will redefine the
           boundaries.
         </Typography>
-        <Button variant='outlined' className='flex-shrink-0'>
+        <Button variant='outlined' className='flex-shrink-0' placeholder={''}>
           join now
         </Button>
       </div>
